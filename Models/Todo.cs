0a82b2e9317d2c732ba96acfe212b79e1bd180d5
Bianca Dollente <biancaadrienne24@gmail.com>
2026-03-30 20:07:23 +0800

    Add Todo model

---
+++ Models/Todo.cs
@@ -0,0 +1,9 @@
+﻿namespace Finals_Q1.Models
+{
+    public class Todo
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public bool Completed { get; set; }
+    }
+}
